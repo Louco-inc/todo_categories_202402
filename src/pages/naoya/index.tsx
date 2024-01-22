494214ba76ca03340657189a0f590c78d0f56b86
mura-work <murata.naoya.work@gmail.com>
2024-01-22 23:27:18 +0900

    D&Dライブラリの導入 D&Dの挙動確認まで

--- src/pages/naoya/index.tsx
+++ src/pages/naoya/index.tsx
@@ -1,18 +1,67 @@
 import Header from "components/header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export default function TodoCategoryListPage() {
+  const [todoList, setTodoList] = useState([]);
   useEffect(() => {
     fetchTodoList();
   }, []);
   const fetchTodoList = async () => {
     const res = await fetch("/api/todo_lists").then((r) => r.json());
-    console.log(res);
+    setTodoList(res);
   };
+
+  const onDragEndTest = (result) => {
+    const items = [...todoList];
+    const deleteItem = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, deleteItem[0]);
+
+    setTodoList(items);
+  };
+
   return (
     <>
       <Header />
-      <div>naoyaのページ</div>
+      <div>
+        <div>
+          <DragDropContext onDragEnd={onDragEndTest}>
+            <Droppable droppableId="droppableId">
+              {(provided) => (
+                <div
+                  className="todoList"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {todoList.map(({ id, title }, index) => {
+                    return (
+                      <Draggable
+                        key={id}
+                        draggableId={String(id)}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            className="testItem"
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <div>
+                              {id}：{title}
+                            </div>
+                          </div>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
+      </div>
     </>
   );
 }
