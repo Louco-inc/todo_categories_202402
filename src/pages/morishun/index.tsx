3817fa37bb9df82d5437d6940a0bf2ca3883dbcd
MoriShun89 <s.moririn0724@gmail.com>
2024-02-11 16:27:19 +0900

    [feat]ドラッグ&ドロップで正常に入れ替えできない問題の修正

--- src/pages/morishun/index.tsx
+++ src/pages/morishun/index.tsx
@@ -46,11 +46,16 @@ const reorder = (
 // 日付を「yyyy-mm-dd」にフォーマット
 // 参考：https://ribbit.konomi.app/blog/javascript-date-format/
 const getFormattedDate = (date: Date): string => {
-  return date.toISOString().split("T")[0];
+  // Invalid Dateでない（正常な日付型の値）場合、フォーマットをかける
+  // 参考：https://b.lollipop.onl/elgxqnm9p/
+  if (!Number.isNaN(date.getDate())) {
+    return date.toISOString().split("T")[0];
+  }
+  return "yyyy-mm-dd"; // 仮置き
 };
 
 export default function TodoCategoryListPage(): JSX.Element {
-  const [todoLists, setTodoLists] = useState<TodoType[]>([]);
+  const [todoLists, setTodoLists] = useState<TodoType[][]>([[], [], []]);
 
   useEffect(() => {
     const init = async (): Promise<void> => {
@@ -60,11 +65,29 @@ export default function TodoCategoryListPage(): JSX.Element {
   }, []);
 
   const fetchTodoList = async (): Promise<void> => {
-    const lists: TodoType[] = await fetch("/api/todo_lists").then(
+    const todoListsAll: TodoType[] = await fetch("/api/todo_lists").then(
       async (r) => await r.json()
     );
-    console.log(lists);
-    setTodoLists(lists);
+    const todoList = todoListsAll
+      .filter((todo: TodoType) => todo.status === "todo")
+      .map((todo: TodoType) => ({
+        ...todo,
+        slug: `todo-${todo.id}`,
+      }));
+    const inprogressList = todoListsAll
+      .filter((todo: TodoType) => todo.status === "inprogress")
+      .map((todo: TodoType) => ({
+        ...todo,
+        slug: `inprogress-${todo.id}`,
+      }));
+    const doneList = todoListsAll
+      .filter((todo: TodoType) => todo.status === "done")
+      .map((todo: TodoType) => ({
+        ...todo,
+        slug: `done-${todo.id}`,
+      }));
+
+    setTodoLists([todoList, inprogressList, doneList]);
   };
 
   const onDragEnd = (result: DropResult): void => {
@@ -72,13 +95,44 @@ export default function TodoCategoryListPage(): JSX.Element {
       return;
     }
 
-    const items = reorder(
-      todoLists,
+    // TodoListsの配列の中身の順番を入れ替える
+    // ドラッグ対象のTodoのstatusのもののみ取り出す
+    // todo:todoLists[0]を入れ替える
+    // inprogress:todoLists[1]を入れ替える
+    // done:todoLists[2]を入れ替える
+    let targetList;
+    switch (result.destination?.droppableId) {
+      case "todo":
+        targetList = todoLists[0];
+        break;
+      case "inprogress":
+        targetList = todoLists[1];
+        break;
+      case "done":
+        targetList = todoLists[2];
+        break;
+    }
+    if (targetList === undefined) return;
+    const newTargetList = reorder(
+      targetList,
       result.source.index,
       result.destination.index
     );
+    let todoListTmp: TodoType[][] = [];
+    switch (result.destination?.droppableId) {
+      case "todo":
+        todoListTmp = [newTargetList, todoLists[1], todoLists[2]];
+        break;
+      case "inprogress":
+        todoListTmp = [todoLists[0], newTargetList, todoLists[2]];
+        break;
 
-    setTodoLists(items);
+      case "done":
+        todoListTmp = [todoLists[0], todoLists[1], newTargetList];
+        break;
+    }
+
+    setTodoLists(todoListTmp);
   };
 
   return (
@@ -87,7 +141,7 @@ export default function TodoCategoryListPage(): JSX.Element {
       <div className="flex justify-around m-8"></div>
       <DragDropContext onDragEnd={onDragEnd}>
         <HStack className="flex justify-between content-center">
-          {["todo", "inprogress", "done"].map((slug) => {
+          {["todo", "inprogress", "done"].map((slug, index) => {
             return (
               <div key={slug} className="bg-dashboard-color w-80">
                 <div className="flex justify-between">
@@ -101,62 +155,60 @@ export default function TodoCategoryListPage(): JSX.Element {
                 <Droppable droppableId={slug}>
                   {(provided) => (
                     <div {...provided.droppableProps} ref={provided.innerRef}>
-                      {todoLists
-                        .filter((todo) => todo.status === slug)
-                        .map((todo, index) => {
-                          return (
-                            <Draggable
-                              key={todo.id}
-                              draggableId={`${slug}-${todo.id}`}
-                              index={index}
-                            >
-                              {(provided) => (
-                                <div
-                                  ref={provided.innerRef}
-                                  {...provided.draggableProps}
-                                  {...provided.dragHandleProps}
-                                >
-                                  <Card className="bg-white m-4 p-4">
-                                    <CardBody>
-                                      <HStack className="flex justify-between">
-                                        <div>
-                                          <Text>{todo.status}</Text>
-                                          {todo.categories.map((category) => {
-                                            return (
-                                              <Tag
-                                                size="sm"
-                                                key={category.id}
-                                                colorScheme={category.color}
-                                              >
-                                                <TagLabel>
-                                                  {category.name}
-                                                </TagLabel>
-                                              </Tag>
-                                            );
-                                          })}
-                                          <Text className="font-bold">
-                                            {todo.title}
-                                          </Text>
-                                          <Text>
-                                            {getFormattedDate(
-                                              new Date(todo.completionDate)
-                                            )}
-                                          </Text>
-                                        </div>
-                                        <IconButton
-                                          variant="unstyled"
-                                          aria-label="Search database"
-                                          icon={<DeleteIcon />}
-                                          onClick={() => {}}
-                                        />
-                                      </HStack>
-                                    </CardBody>
-                                  </Card>
-                                </div>
-                              )}
-                            </Draggable>
-                          );
-                        })}
+                      {todoLists[index].map((todo, index) => {
+                        return (
+                          <Draggable
+                            key={todo.id}
+                            draggableId={`${slug}-${todo.id}`}
+                            index={index}
+                          >
+                            {(provided) => (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                              >
+                                <Card className="bg-white m-4 p-4">
+                                  <CardBody>
+                                    <HStack className="flex justify-between">
+                                      <div>
+                                        <Text>{todo.status}</Text>
+                                        {todo.categories.map((category) => {
+                                          return (
+                                            <Tag
+                                              size="sm"
+                                              key={category.id}
+                                              colorScheme={category.color}
+                                            >
+                                              <TagLabel>
+                                                {category.name}
+                                              </TagLabel>
+                                            </Tag>
+                                          );
+                                        })}
+                                        <Text className="font-bold">
+                                          {todo.title}
+                                        </Text>
+                                        <Text>
+                                          {getFormattedDate(
+                                            new Date(todo.completionDate)
+                                          )}
+                                        </Text>
+                                      </div>
+                                      <IconButton
+                                        variant="unstyled"
+                                        aria-label="Search database"
+                                        icon={<DeleteIcon />}
+                                        onClick={() => {}}
+                                      />
+                                    </HStack>
+                                  </CardBody>
+                                </Card>
+                              </div>
+                            )}
+                          </Draggable>
+                        );
+                      })}
                       {provided.placeholder}
                     </div>
                   )}
